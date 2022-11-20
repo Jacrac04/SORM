f45e298897a70d292f90c6572091075e8d0d1211
Jacrac04 <62761393+Jacrac04@users.noreply.github.com>
2022-11-20 16:25:54 +0000

    Remove ignored files

--- db.py
+++
@@ -1,7 +0,0 @@
-from sorm.orm import ORM
-SETTINGS = {
-            'type': 'SQLITE',
-            'databaseURI': 'dev.sqlite',
-            'isolation_level': 'DEFERRED'
-        }  
-db = ORM(SETTINGS)
